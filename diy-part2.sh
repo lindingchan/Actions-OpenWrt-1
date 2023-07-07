941276c5695c982e3affc4b4f34306a15400741d
lindingchan <73469714+lindingchan@users.noreply.github.com>
2023-07-07 20:04:43 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.0.31/g' package/base-files/files/bin/config_generate
 
 sed -i "s/OpenWrt /Tlan compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
